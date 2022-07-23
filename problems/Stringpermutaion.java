579e71992b45a1d9b2aab80f57f9855ab28dacbe
shivam <64252021+itsprogrammingwithme@users.noreply.github.com>
2022-07-23 11:05:53 +0530

    string permutaion

---
+++ problems/Stringpermutaion.java
@@ -0,0 +1,26 @@
+package problems;
+import java.util.*;;
+public class Stringpermutaion {
+    public static void main(String[] args) {
+        String s="abcd";
+        int arr[]={2,4,3,1};
+        System.out.println(stringstringpermutation(s, arr)); //dacb
+
+    }
+    public static String stringstringpermutation(String s,int arr[]) {
+        HashMap<Integer,Character>map=new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], s.charAt(i));
+        }
+        String ans="";
+
+        for(int i :map.keySet()){
+            ans=ans+map.get(i);
+        }
+
+        return ans;
+        
+    }
+    
+}
